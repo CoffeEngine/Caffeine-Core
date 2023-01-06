73929e72cd8610451e0b18cf03cb0cfd78697d4e
ruann <ruann@MAHLIK>
2023-01-06 20:10:32 -0300

    add project uuid

--- premake5.lua
+++ premake5.lua
@@ -5,7 +5,8 @@ workspace "Caffeine"
     project "Caffeine-Core"   
         kind "StaticLib"  
         language "C"   
-        
+        uuid "6f788d01-ed35-4f0b-bbdd-860c7a311d9b"
+
         targetdir "build/%{prj.name}/bin/%{cfg.buildcfg}"
         objdir "build/%{prj.name}/obj/%{cfg.buildcfg}"
         
